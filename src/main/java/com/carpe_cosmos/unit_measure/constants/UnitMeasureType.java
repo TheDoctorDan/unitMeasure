12f74d00b91731c3ec34af557d5365be1281bad9
Dan Gleeson <dgleeson1721@gmail.com>
2020-06-08 08:20:43 -0500

    rename pkg.  Use UnitMeasureException.

--- src/main/java/com/carpe_cosmos/unit_measure/constants/UnitMeasureType.java
+++ src/main/java/com/carpe_cosmos/unit_measure/constants/UnitMeasureType.java
@@ -1,9 +1,9 @@
-package com.carpeCosmos.unitMeasure.constants;
+package com.carpe_cosmos.unit_measure.constants;
 
-import com.carpeCosmos.unitMeasure.domain.SimpleUnitMeasurement;
+import com.carpe_cosmos.unit_measure.domain.SimpleUnitMeasurement;
+import com.carpe_cosmos.unit_measure.exceptions.UnitMeasureException;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 public interface UnitMeasureType {
 
@@ -21,12 +21,12 @@ public interface UnitMeasureType {
     UnitPrefix getDefaultUnitPrefix();
 
 
-    static <T extends Enum<T> & UnitMeasureType> T findBySymbol(Class<T> enumClass, String symbol) throws NoSuchElementException {
+    static <T extends Enum<T> & UnitMeasureType> T findBySymbol(Class<T> enumClass, String symbol) throws UnitMeasureException {
         for (T unitDimensionType : enumClass.getEnumConstants()) {
             if (unitDimensionType.getSymbol().equals(symbol))
                 return unitDimensionType;
         }
-        throw new NoSuchElementException("No " + enumClass.getSimpleName() + " Enum with symbol of " + symbol + ".");
+        throw new UnitMeasureException("No " + enumClass.getSimpleName() + " Enum with symbol of " + symbol + ".");
     }
 
 
