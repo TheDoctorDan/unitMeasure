7ad427fc9218b02074a814303f06a0fe5f1299e5
Dan Gleeson <dgleeson1721@gmail.com>
2020-06-08 08:33:30 -0500

    Use static imports

--- src/test/java/com/carpe_cosmos/unit_measure/constants/UnitMeasureBaseTypeTest.java
+++ src/test/java/com/carpe_cosmos/unit_measure/constants/UnitMeasureBaseTypeTest.java
@@ -2,14 +2,33 @@ package com.carpe_cosmos.unit_measure.constants;
 
 import com.carpe_cosmos.unit_measure.domain.SimpleUnitMeasurement;
 import com.carpe_cosmos.unit_measure.exceptions.UnitMeasureException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
+import static com.carpe_cosmos.unit_measure.constants.FundamentalMeasurementType.AMOUNT_OF_SUBSTANCE;
+import static com.carpe_cosmos.unit_measure.constants.FundamentalMeasurementType.ELECTRICAL_CURRENT;
+import static com.carpe_cosmos.unit_measure.constants.FundamentalMeasurementType.LENGTH;
+import static com.carpe_cosmos.unit_measure.constants.FundamentalMeasurementType.LUMINOUS_INTENSITY;
+import static com.carpe_cosmos.unit_measure.constants.FundamentalMeasurementType.MASS;
+import static com.carpe_cosmos.unit_measure.constants.FundamentalMeasurementType.TEMPERATURE;
+import static com.carpe_cosmos.unit_measure.constants.FundamentalMeasurementType.TIME;
+import static com.carpe_cosmos.unit_measure.constants.FundamentalMeasurementType.UNIT_LESS_NUMBER;
+import static com.carpe_cosmos.unit_measure.constants.UnitMeasureBaseType.AMPERE;
+import static com.carpe_cosmos.unit_measure.constants.UnitMeasureBaseType.CANDELA;
+import static com.carpe_cosmos.unit_measure.constants.UnitMeasureBaseType.EACH;
+import static com.carpe_cosmos.unit_measure.constants.UnitMeasureBaseType.GRAM;
+import static com.carpe_cosmos.unit_measure.constants.UnitMeasureBaseType.KELVIN;
+import static com.carpe_cosmos.unit_measure.constants.UnitMeasureBaseType.METER;
+import static com.carpe_cosmos.unit_measure.constants.UnitMeasureBaseType.MOLE;
+import static com.carpe_cosmos.unit_measure.constants.UnitMeasureBaseType.SECOND;
+import static com.carpe_cosmos.unit_measure.constants.UnitMeasureBaseType.findByFundamentalMeasurementType;
+import static com.carpe_cosmos.unit_measure.constants.UnitMeasureType.findBySymbol;
 import static java.util.Collections.singletonList;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class UnitMeasureBaseTypeTest {
@@ -17,130 +36,130 @@ public class UnitMeasureBaseTypeTest {
 
     @Test
     public void isBaseUnitDimensionTest() {
-        Assertions.assertTrue(UnitMeasureBaseType.METER.isBaseUnitDimension());
-        Assertions.assertTrue(UnitMeasureBaseType.GRAM.isBaseUnitDimension());
-        Assertions.assertTrue(UnitMeasureBaseType.SECOND.isBaseUnitDimension());
-        Assertions.assertTrue(UnitMeasureBaseType.AMPERE.isBaseUnitDimension());
-        Assertions.assertTrue(UnitMeasureBaseType.KELVIN.isBaseUnitDimension());
-        Assertions.assertTrue(UnitMeasureBaseType.MOLE.isBaseUnitDimension());
-        Assertions.assertTrue(UnitMeasureBaseType.CANDELA.isBaseUnitDimension());
-        Assertions.assertTrue(UnitMeasureBaseType.EACH.isBaseUnitDimension());
+        assertTrue(METER.isBaseUnitDimension());
+        assertTrue(GRAM.isBaseUnitDimension());
+        assertTrue(SECOND.isBaseUnitDimension());
+        assertTrue(AMPERE.isBaseUnitDimension());
+        assertTrue(KELVIN.isBaseUnitDimension());
+        assertTrue(MOLE.isBaseUnitDimension());
+        assertTrue(CANDELA.isBaseUnitDimension());
+        assertTrue(EACH.isBaseUnitDimension());
     }
 
     @Test
     public void findBySymbolTestNormalSet() throws Exception {
-        Assertions.assertEquals(UnitMeasureBaseType.METER, UnitMeasureType.findBySymbol(UnitMeasureBaseType.class, "m"));
-        Assertions.assertEquals(UnitMeasureBaseType.GRAM, UnitMeasureType.findBySymbol(UnitMeasureBaseType.class, "g"));
-        Assertions.assertEquals(UnitMeasureBaseType.SECOND, UnitMeasureType.findBySymbol(UnitMeasureBaseType.class, "s"));
-        Assertions.assertEquals(UnitMeasureBaseType.AMPERE, UnitMeasureType.findBySymbol(UnitMeasureBaseType.class, "A"));
-        Assertions.assertEquals(UnitMeasureBaseType.KELVIN, UnitMeasureType.findBySymbol(UnitMeasureBaseType.class, "K"));
-        Assertions.assertEquals(UnitMeasureBaseType.MOLE, UnitMeasureType.findBySymbol(UnitMeasureBaseType.class, "mole"));
-        Assertions.assertEquals(UnitMeasureBaseType.CANDELA, UnitMeasureType.findBySymbol(UnitMeasureBaseType.class, "cd"));
-        Assertions.assertEquals(UnitMeasureBaseType.EACH, UnitMeasureType.findBySymbol(UnitMeasureBaseType.class, "ea"));
+        assertEquals(METER, findBySymbol(UnitMeasureBaseType.class, "m"));
+        assertEquals(GRAM, findBySymbol(UnitMeasureBaseType.class, "g"));
+        assertEquals(SECOND, findBySymbol(UnitMeasureBaseType.class, "s"));
+        assertEquals(AMPERE, findBySymbol(UnitMeasureBaseType.class, "A"));
+        assertEquals(KELVIN, findBySymbol(UnitMeasureBaseType.class, "K"));
+        assertEquals(MOLE, findBySymbol(UnitMeasureBaseType.class, "mole"));
+        assertEquals(CANDELA, findBySymbol(UnitMeasureBaseType.class, "cd"));
+        assertEquals(EACH, findBySymbol(UnitMeasureBaseType.class, "ea"));
     }
 
     @Test
     public void findBySymbolTestFailure() {
         Throwable thrown = assertThrows(UnitMeasureException.class, () ->
-                UnitMeasureType.findBySymbol(UnitMeasureBaseType.class, "z"));
+                findBySymbol(UnitMeasureBaseType.class, "z"));
         assertEquals("No UnitMeasureBaseType Enum with symbol of z.", thrown.getMessage());
     }
 
     @Test
     public void findByFundamentalMeasurementTypeTestNormalSet() throws Exception {
-        Assertions.assertEquals(UnitMeasureBaseType.METER, UnitMeasureBaseType.findByFundamentalMeasurementType(FundamentalMeasurementType.LENGTH));
-        Assertions.assertEquals(UnitMeasureBaseType.GRAM, UnitMeasureBaseType.findByFundamentalMeasurementType(FundamentalMeasurementType.MASS));
-        Assertions.assertEquals(UnitMeasureBaseType.SECOND, UnitMeasureBaseType.findByFundamentalMeasurementType(FundamentalMeasurementType.TIME));
-        Assertions.assertEquals(UnitMeasureBaseType.AMPERE, UnitMeasureBaseType.findByFundamentalMeasurementType(FundamentalMeasurementType.ELECTRICAL_CURRENT));
-        Assertions.assertEquals(UnitMeasureBaseType.KELVIN, UnitMeasureBaseType.findByFundamentalMeasurementType(FundamentalMeasurementType.TEMPERATURE));
-        Assertions.assertEquals(UnitMeasureBaseType.MOLE, UnitMeasureBaseType.findByFundamentalMeasurementType(FundamentalMeasurementType.AMOUNT_OF_SUBSTANCE));
-        Assertions.assertEquals(UnitMeasureBaseType.CANDELA, UnitMeasureBaseType.findByFundamentalMeasurementType(FundamentalMeasurementType.LUMINOUS_INTENSITY));
-        Assertions.assertEquals(UnitMeasureBaseType.EACH, UnitMeasureBaseType.findByFundamentalMeasurementType(FundamentalMeasurementType.UNIT_LESS_NUMBER));
+        assertEquals(METER, findByFundamentalMeasurementType(LENGTH));
+        assertEquals(GRAM, findByFundamentalMeasurementType(MASS));
+        assertEquals(SECOND, findByFundamentalMeasurementType(TIME));
+        assertEquals(AMPERE, findByFundamentalMeasurementType(ELECTRICAL_CURRENT));
+        assertEquals(KELVIN, findByFundamentalMeasurementType(TEMPERATURE));
+        assertEquals(MOLE, findByFundamentalMeasurementType(AMOUNT_OF_SUBSTANCE));
+        assertEquals(CANDELA, findByFundamentalMeasurementType(LUMINOUS_INTENSITY));
+        assertEquals(EACH, findByFundamentalMeasurementType(UNIT_LESS_NUMBER));
     }
 
     @Test
     public void findByFundamentalMeasurementTypeTestFailure() {
         Throwable thrown = assertThrows(UnitMeasureException.class, () ->
-                UnitMeasureBaseType.findByFundamentalMeasurementType(null));
+                findByFundamentalMeasurementType(null));
         assertEquals("No UnitMeasureBaseType Enum with fundamentalMeasurementType of null.", thrown.getMessage());
     }
 
     @Test
     public void getNumeratorDimensionList() {
-        Assertions.assertArrayEquals(
+        assertArrayEquals(
                 singletonList(SimpleUnitMeasurement.builder()
-                        .unitMeasureType(UnitMeasureBaseType.METER).build()).toArray(),
-                UnitMeasureBaseType.METER.getNumeratorSimpleUnitMeasurementList().toArray());
+                        .unitMeasureType(METER).build()).toArray(),
+                METER.getNumeratorSimpleUnitMeasurementList().toArray());
 
-        Assertions.assertArrayEquals(
+        assertArrayEquals(
                 singletonList(SimpleUnitMeasurement.builder()
-                        .unitMeasureType(UnitMeasureBaseType.GRAM).build()).toArray(),
-                UnitMeasureBaseType.GRAM.getNumeratorSimpleUnitMeasurementList().toArray());
+                        .unitMeasureType(GRAM).build()).toArray(),
+                GRAM.getNumeratorSimpleUnitMeasurementList().toArray());
 
-        Assertions.assertArrayEquals(
+        assertArrayEquals(
                 singletonList(SimpleUnitMeasurement.builder()
-                        .unitMeasureType(UnitMeasureBaseType.SECOND).build()).toArray(),
-                UnitMeasureBaseType.SECOND.getNumeratorSimpleUnitMeasurementList().toArray());
+                        .unitMeasureType(SECOND).build()).toArray(),
+                SECOND.getNumeratorSimpleUnitMeasurementList().toArray());
 
-        Assertions.assertArrayEquals(
+        assertArrayEquals(
                 singletonList(SimpleUnitMeasurement.builder()
-                        .unitMeasureType(UnitMeasureBaseType.AMPERE).build()).toArray(),
-                UnitMeasureBaseType.AMPERE.getNumeratorSimpleUnitMeasurementList().toArray());
+                        .unitMeasureType(AMPERE).build()).toArray(),
+                AMPERE.getNumeratorSimpleUnitMeasurementList().toArray());
 
-        Assertions.assertArrayEquals(
+        assertArrayEquals(
                 singletonList(SimpleUnitMeasurement.builder()
-                        .unitMeasureType(UnitMeasureBaseType.KELVIN).build()).toArray(),
-                UnitMeasureBaseType.KELVIN.getNumeratorSimpleUnitMeasurementList().toArray());
+                        .unitMeasureType(KELVIN).build()).toArray(),
+                KELVIN.getNumeratorSimpleUnitMeasurementList().toArray());
 
-        Assertions.assertArrayEquals(
+        assertArrayEquals(
                 singletonList(SimpleUnitMeasurement.builder()
-                        .unitMeasureType(UnitMeasureBaseType.MOLE).build()).toArray(),
-                UnitMeasureBaseType.MOLE.getNumeratorSimpleUnitMeasurementList().toArray());
+                        .unitMeasureType(MOLE).build()).toArray(),
+                MOLE.getNumeratorSimpleUnitMeasurementList().toArray());
 
-        Assertions.assertArrayEquals(
+        assertArrayEquals(
                 singletonList(SimpleUnitMeasurement.builder()
-                        .unitMeasureType(UnitMeasureBaseType.CANDELA).build()).toArray(),
-                UnitMeasureBaseType.CANDELA.getNumeratorSimpleUnitMeasurementList().toArray());
+                        .unitMeasureType(CANDELA).build()).toArray(),
+                CANDELA.getNumeratorSimpleUnitMeasurementList().toArray());
 
-        Assertions.assertArrayEquals(
+        assertArrayEquals(
                 singletonList(SimpleUnitMeasurement.builder()
-                        .unitMeasureType(UnitMeasureBaseType.EACH).build()).toArray(),
-                UnitMeasureBaseType.EACH.getNumeratorSimpleUnitMeasurementList().toArray());
+                        .unitMeasureType(EACH).build()).toArray(),
+                EACH.getNumeratorSimpleUnitMeasurementList().toArray());
 
     }
 
     @Test
     public void getDenominatorDimensionList() {
-        Assertions.assertArrayEquals(
+        assertArrayEquals(
                 Collections.emptyList().toArray(),
-                UnitMeasureBaseType.METER.getDenominatorSimpleUnitMeasurementList().toArray());
+                METER.getDenominatorSimpleUnitMeasurementList().toArray());
 
-        Assertions.assertArrayEquals(
+        assertArrayEquals(
                 Collections.emptyList().toArray(),
-                UnitMeasureBaseType.EACH.getDenominatorSimpleUnitMeasurementList().toArray());
+                EACH.getDenominatorSimpleUnitMeasurementList().toArray());
     }
 
     @Test
     public void getDefaultUnitPrefix() {
-        Assertions.assertEquals(UnitPrefix.KILO, UnitMeasureBaseType.GRAM.getDefaultUnitPrefix());
-        Assertions.assertEquals(UnitPrefix.UNO, UnitMeasureBaseType.METER.getDefaultUnitPrefix());
-        Assertions.assertEquals(UnitPrefix.UNO, UnitMeasureBaseType.SECOND.getDefaultUnitPrefix());
-        Assertions.assertEquals(UnitPrefix.UNO, UnitMeasureBaseType.AMPERE.getDefaultUnitPrefix());
-        Assertions.assertEquals(UnitPrefix.UNO, UnitMeasureBaseType.KELVIN.getDefaultUnitPrefix());
-        Assertions.assertEquals(UnitPrefix.UNO, UnitMeasureBaseType.MOLE.getDefaultUnitPrefix());
-        Assertions.assertEquals(UnitPrefix.UNO, UnitMeasureBaseType.CANDELA.getDefaultUnitPrefix());
-        Assertions.assertEquals(UnitPrefix.UNO, UnitMeasureBaseType.EACH.getDefaultUnitPrefix());
+        assertEquals(UnitPrefix.KILO, GRAM.getDefaultUnitPrefix());
+        assertEquals(UnitPrefix.UNO, METER.getDefaultUnitPrefix());
+        assertEquals(UnitPrefix.UNO, SECOND.getDefaultUnitPrefix());
+        assertEquals(UnitPrefix.UNO, AMPERE.getDefaultUnitPrefix());
+        assertEquals(UnitPrefix.UNO, KELVIN.getDefaultUnitPrefix());
+        assertEquals(UnitPrefix.UNO, MOLE.getDefaultUnitPrefix());
+        assertEquals(UnitPrefix.UNO, CANDELA.getDefaultUnitPrefix());
+        assertEquals(UnitPrefix.UNO, EACH.getDefaultUnitPrefix());
     }
 
     @Test
     public void getFundamentalMeasurementType() {
-        Assertions.assertEquals(FundamentalMeasurementType.MASS, UnitMeasureBaseType.GRAM.getFundamentalMeasurementType());
-        Assertions.assertEquals(FundamentalMeasurementType.LENGTH, UnitMeasureBaseType.METER.getFundamentalMeasurementType());
-        Assertions.assertEquals(FundamentalMeasurementType.TIME, UnitMeasureBaseType.SECOND.getFundamentalMeasurementType());
-        Assertions.assertEquals(FundamentalMeasurementType.ELECTRICAL_CURRENT, UnitMeasureBaseType.AMPERE.getFundamentalMeasurementType());
-        Assertions.assertEquals(FundamentalMeasurementType.TEMPERATURE, UnitMeasureBaseType.KELVIN.getFundamentalMeasurementType());
-        Assertions.assertEquals(FundamentalMeasurementType.AMOUNT_OF_SUBSTANCE, UnitMeasureBaseType.MOLE.getFundamentalMeasurementType());
-        Assertions.assertEquals(FundamentalMeasurementType.LUMINOUS_INTENSITY, UnitMeasureBaseType.CANDELA.getFundamentalMeasurementType());
-        Assertions.assertEquals(FundamentalMeasurementType.UNIT_LESS_NUMBER, UnitMeasureBaseType.EACH.getFundamentalMeasurementType());
+        assertEquals(MASS, GRAM.getFundamentalMeasurementType());
+        assertEquals(LENGTH, METER.getFundamentalMeasurementType());
+        assertEquals(TIME, SECOND.getFundamentalMeasurementType());
+        assertEquals(ELECTRICAL_CURRENT, AMPERE.getFundamentalMeasurementType());
+        assertEquals(TEMPERATURE, KELVIN.getFundamentalMeasurementType());
+        assertEquals(AMOUNT_OF_SUBSTANCE, MOLE.getFundamentalMeasurementType());
+        assertEquals(LUMINOUS_INTENSITY, CANDELA.getFundamentalMeasurementType());
+        assertEquals(UNIT_LESS_NUMBER, EACH.getFundamentalMeasurementType());
     }
 }
