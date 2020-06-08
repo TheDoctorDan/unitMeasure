6a76af11fb7e6d19c7dbc91cf9ffc2d9ae612759
Dan Gleeson <dgleeson1721@gmail.com>
2020-06-08 08:13:50 -0500

    rename package

--- src/main/java/com/carpe_cosmos/unit_measure/constants/FundamentalMeasurementType.java
+++ src/main/java/com/carpe_cosmos/unit_measure/constants/FundamentalMeasurementType.java
@@ -1,6 +1,5 @@
-package com.carpeCosmos.unitMeasure.constants;
+package com.carpe_cosmos.unit_measure.constants;
 
-public enum FundamentalMeasurementType
-{
+public enum FundamentalMeasurementType {
     LENGTH, MASS, TIME, ELECTRICAL_CURRENT, TEMPERATURE, AMOUNT_OF_SUBSTANCE, LUMINOUS_INTENSITY, UNIT_LESS_NUMBER
 }
