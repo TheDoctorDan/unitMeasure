9c5ab36a8388b5ea4cce19738792b2ff3473d959
Dan Gleeson <dgleeson1721@gmail.com>
2020-06-07 21:06:04 -0500

    Initial commit

---
+++ src/main/java/com/carpeCosmos/unitMeasure/exceptions/UnitMeasureException.java
@@ -0,0 +1,22 @@
+package com.carpeCosmos.unitMeasure.exceptions;
+
+public class UnitMeasureException extends Exception {
+    public UnitMeasureException() {
+    }
+
+    public UnitMeasureException(String message) {
+        super(message);
+    }
+
+    public UnitMeasureException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UnitMeasureException(Throwable cause) {
+        super(cause);
+    }
+
+    public UnitMeasureException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ src/main/java/com/carpeCosmos/unitMeasure/exceptions/UnitPrefixException.java
@@ -0,0 +1,22 @@
+package com.carpeCosmos.unitMeasure.exceptions;
+
+public class UnitPrefixException extends Exception {
+    public UnitPrefixException() {
+    }
+
+    public UnitPrefixException(String message) {
+        super(message);
+    }
+
+    public UnitPrefixException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UnitPrefixException(Throwable cause) {
+        super(cause);
+    }
+
+    public UnitPrefixException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
